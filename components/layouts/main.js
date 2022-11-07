d4e2583a44440117061730f5b0c6ecf27a27dc0d
andresflorez16 <andresflorez.dev@gmail.com>
2022-11-07 11:38:10 -0500

    card image

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,12 +15,12 @@ export const Layout = ({ children }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@andresflorezdev" />
         <meta name="twitter:creator" content="@andresflorezdev" />
-        <meta name="twitter:image" content={`${originPath}/andrew.jpg`} />
+        <meta name="twitter:image" content={`${originPath}/card.jpeg`} />
 				<meta name='author' content="Andrés Florez" />
 				<meta name='description' content="Andrés Florez Website" />
 				<meta property="og:site_name" content="Andrés Florez" />
 				<meta property="og:description" content="Andrés Florez Website" />
-				<meta property="og:image" content={`${originPath}/andrew.jpg`} />
+				<meta property="og:image" content={`${originPath}/card.jpeg`} />
         <link rel="apple-touch-icon" href="andres.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
