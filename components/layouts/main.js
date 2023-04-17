ca42aa615c367b70b1b5fe02a62306386b8746bb
andresflorez16 <andresflorez.dev@gmail.com>
2023-04-16 23:47:29 -0500

    eslint code

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -11,18 +11,18 @@ export const Layout = ({ children }) => {
       minH='100vh'
     >
       <Head>
-        <meta name="twitter:title" content="Andrés Florez" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@andresflorezdev" />
-        <meta name="twitter:creator" content="@andresflorezdev" />
-        <meta name="twitter:image" content={`${originPath}/card.jpeg`} />
-				<meta name='author' content="Andrés Florez" />
-				<meta name='description' content="Andrés Florez Website" />
-				<meta property="og:site_name" content="Andrés Florez" />
-				<meta property="og:description" content="Andrés Florez Website" />
-				<meta property="og:image" content={`${originPath}/card.jpeg`} />
-        <link rel="apple-touch-icon" href="andres.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name='twitter:title' content='Andrés Florez' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@andresflorezdev' />
+        <meta name='twitter:creator' content='@andresflorezdev' />
+        <meta name='twitter:image' content={`${originPath}/card.jpeg`} />
+        <meta name='author' content='Andrés Florez' />
+        <meta name='description' content='Andrés Florez Website' />
+        <meta property='og:site_name' content='Andrés Florez' />
+        <meta property='og:description' content='Andrés Florez Website' />
+        <meta property='og:image' content={`${originPath}/card.jpeg`} />
+        <link rel='apple-touch-icon' href='andres.png' />
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
       </Head>
       <Navbar />
       {children}
